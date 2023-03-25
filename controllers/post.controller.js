83f56bb0be656143294e81b35a295ee8ea79da60
sonleminh <lms.smile2000@gmail.com>
2023-03-26 01:47:20 +0700

    push

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,30 +1,55 @@
 const { default: mongoose } = require('mongoose');
 const Image = require('../models/image.model');
 
+// const getAllImage = async (req, res) => {
+//   try {
+//     const imageList = await Image.find({});
+//     return res.status(200).json(imageList);
+//   } catch (error) {
+//     throw new Error('Not Found Image List');
+//   }
+// };
+
 const getAllImage = async (req, res) => {
   try {
-    const imageList = await Image.find({});
-    return res.status(200).json(imageList);
+    const randomImage = await Image.aggregate([
+      {
+        $sample: { size: 50 },
+      },
+    ]);
+    return res.status(200).json(randomImage);
   } catch (error) {
-    throw new Error('Not Found Image List');
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+const getImages = async (req, res) => {
+  try {
+    const images = await Image.find({
+      type: 'image',
+    });
+    return res.status(200).json(images);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+const getVideos = async (req, res) => {
+  try {
+    const images = await Image.find({
+      type: 'video',
+    });
+    return res.status(200).json(images);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
   }
 };
 
 const createImage = async (req, res) => {
-  // console.log(req.body);
   const data = req.body.data;
   try {
     await Image.insertMany(data);
     res.status(200).json({ message: 'Post successfully' });
-
-    // let newPost;
-    // imageData.map(async (item) => {
-    //   newPost = new Image({
-    //     item,
-    //   });
-    // });
-    // console.log(newPost);
-    // await newPost.save();
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Something went wrong' });
@@ -33,5 +58,7 @@ const createImage = async (req, res) => {
 
 module.exports = {
   getAllImage,
+  getImages,
+  getVideos,
   createImage,
 };
--- routes/image.route.js
+++ routes/image.route.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const { getAllImage, createImage } = require('../controllers/post.controller');
+const {
+  getAllImage,
+  getImages,
+  getVideos,
+  createImage,
+} = require('../controllers/post.controller');
 const router = express.Router();
 
 router.get('/post', getAllImage);
+router.get('/image', getImages);
+router.get('/video', getVideos);
 router.post('/post', createImage);
 
 module.exports = router;
